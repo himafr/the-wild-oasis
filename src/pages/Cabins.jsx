589959e02c0df9fe1ad88380daf32b80c7c76f7e
hima <ebrahumahmed2003@gmail.com>
2025-07-02 15:32:13 +0300

    feat: integrate Supabase for cabin data fetching and add related dependencies

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [cab, setCab] = useState({});
+  useEffect(() => {
+    async function fetchCabins() {
+      try {
+        const cab = await getCabins();
+        setCab(cab[0]);
+      } catch (error) {
+        console.error("Error fetching cabins:", error);
+      }
+    }
+    fetchCabins();}
+  ,[])
+  if (!Object.keys(cab).length) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src={cab.image} alt="Cabin" />
     </Row>
   );
 }
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://fslvzslmpmavpanfriyi.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbHZ6c2xtcG1hdnBhbmZyaXlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0MzA0MTAsImV4cCI6MjA2NzAwNjQxMH0.EkRCeoFDttIjK_TwiaZAVevfbZuf0mHJZT2aSIMH8bc"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase

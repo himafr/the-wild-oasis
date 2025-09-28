538debc896d6fae3f45c8e1be14763bef5922201
hima <ebrahumahmed2003@gmail.com>
2025-09-28 23:46:49 +0300

    final upload removing fake user

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("hema@sallem.com");
-  const [password, setPassword] = useState("pass1234");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const {isPending,login}=useLogin()
 
   function handleSubmit(e) {

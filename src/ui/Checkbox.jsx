de134bc8451dabefc9abe3fe0bbf9a3d2902fe65
hima <ebrahumahmed2003@gmail.com>
2025-09-29 00:01:59 +0300

    fix small issue

--- src/ui/Checkbox.jsx
+++ src/ui/Checkbox.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import styled from "styled-components";
 
 const StyledCheckbox = styled.div`
@@ -25,7 +26,7 @@ const StyledCheckbox = styled.div`
   }
 `;
 
-function Checkbox({ checked, onChange, disabled = false, id, children }) {
+function CheckBox({ checked, onChange, disabled = false, id, children }) {
   return (
     <StyledCheckbox>
       <input
@@ -40,4 +41,4 @@ function Checkbox({ checked, onChange, disabled = false, id, children }) {
   );
 }
 
-export default Checkbox;
+export default CheckBox;
